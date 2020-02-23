3a47aa08da4c742ed137c45e254236609d2d78be
Ming Fong <mingfong07@hotmail.com>
2020-02-23 01:45:43 -0800

    update for python3

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from elopy import *
 
-i = Implementation
+
+
+#read from a spreadsheet or SQL table
+
+#calculate elo while reading
+
+#return list of players and elo
+
+#display on a webpage
+i = Implementation()
+
